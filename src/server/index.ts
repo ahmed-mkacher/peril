3cf545a27df3c8b0e0ef13fe1a8fd45c4ab55d35
Ahmed Mkacher <mkacher.ahmed@outlook.com>
2026-09-05 07:39:56 +0100

    chore: finished step 2.4

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,17 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+
+  const mq_address = "amqp://guest:guest@localhost:5672/";
+  const mq_conn = await amqp.connect(mq_address);
+
+  if (mq_conn) console.log("Connected Successfully!");
+
+  process.on("SIGINT", () => {
+    console.log("Closing connection now");
+    mq_conn.close();
+  })
 }
 
 main().catch((err) => {
